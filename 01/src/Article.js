51d110e47720fd0c22c38d9a181f6d75e5bd145c
shturnev <sht_job@ukr.net>
2017-11-04 23:22:48 +0200

    перевел на компонент класса, теперь выведем несколько статтей сразу

--- 01/src/Article.js
+++ 01/src/Article.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+//простой stateless компонент
+/*
 export default function Article(props) {
     return (
         <article>
@@ -7,4 +9,56 @@ export default function Article(props) {
             <div className = "text">{props.db.text}</div>
         </article>
     );
+}*/
+
+
+//Перепишем на компонент из класса
+export default class Article extends Component{
+    constructor(props){
+        super(props);
+
+        this.state ={
+            isOpen: false
+        };
+
+        this.toggler = this.toggler.bind(this);
+
+    }
+
+    render(){
+        let {title} = this.props.db;
+
+        return (
+            <article>
+                <h2>{title}</h2>
+                {this.getText()}
+                {this.getBtn()}
+            </article>
+        );
+    }
+
+    getText(){
+        if(!this.state.isOpen){return null;}
+        let text = this.props.db.text;
+        return <div className = "text">{text}</div>
+    }
+
+    getBtn(){
+        let text = this.state.isOpen ? 'скрыть текст' : 'показать текст';
+
+        return (
+            <div style={{'marginTop': '10px'}}>
+                <button onClick={this.toggler}>{text}</button>
+            </div>
+        );
+
+    }
+
+    toggler(){
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+
 }
