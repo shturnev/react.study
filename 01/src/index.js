140f80429e37bc7038bc3dd5120420762a0754b5
shturnev <sht_job@ukr.net>
2017-11-07 16:20:19 +0200

    Добавил data-picker

--- 01/src/index.js
+++ 01/src/index.js
@@ -1,9 +1,20 @@
 import {articles as db} from './db.js';
 import React from 'react';
 import {render} from 'react-dom';
+
+//components
 import Articles from './components/Articles';
+import DataPicker from './components/DataPicker';
+
+//styles
 import './style/test.css';
 
-render(<Articles />, document.getElementById('root'));
+render(
+    <div>
+        <DataPicker/>
+        <Articles />
+    </div>,
+    document.getElementById('root')
+);
 
 
--- 01/webpack.config.js
+++ 01/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                include: path.join(__dirname, 'src'),
+                // include: path.join(__dirname, 'src'),
                 loaders: ['style-loader', 'css-loader']
                 /*use: [
                     {
